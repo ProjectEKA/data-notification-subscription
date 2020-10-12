c1f4ba735afbadc80b3fd64abcc272e30f51fc71
Angshuman Sarkar <angshuonline@gmail.com>
2020-10-12 10:17:34 +0530

    1046 | Fixing deserialization issue because of pkg diff

--- src/main/java/in/projecteka/datanotificationsubscription/hipLink/HipLinkNotificationListener.java
+++ src/main/java/in/projecteka/datanotificationsubscription/hipLink/HipLinkNotificationListener.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import in.projecteka.datanotificationsubscription.HIUSubscriptionManager;
 import in.projecteka.datanotificationsubscription.MessageListenerContainerFactory;
-import in.projecteka.datanotificationsubscription.SampleNotificationPublisher;
 import in.projecteka.consentmanager.common.TraceableMessage;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
@@ -19,7 +18,7 @@ import static in.projecteka.datanotificationsubscription.common.Constants.HIP_LI
 
 @AllArgsConstructor
 public class HipLinkNotificationListener {
-    private static final Logger logger = LoggerFactory.getLogger(SampleNotificationPublisher.class);
+    private static final Logger logger = LoggerFactory.getLogger(HipLinkNotificationListener.class);
     private final MessageListenerContainerFactory messageListenerContainerFactory;
     private final Jackson2JsonMessageConverter converter;
     private final HIUSubscriptionManager subscriptionManager;
@@ -29,11 +28,12 @@ public class HipLinkNotificationListener {
         var mlc = messageListenerContainerFactory.createMessageListenerContainer(HIP_LINK_QUEUE);
         MessageListener messageListener = message -> {
             try {
-                TraceableMessage traceableMessage = (TraceableMessage) converter.fromMessage(message);
+                logger.info("Message from queue: {} ", message);
                 ObjectMapper mapper = new ObjectMapper();
                 mapper.registerModule(new JavaTimeModule());
+                TraceableMessage traceableMessage = mapper.readValue(new String(message.getBody(), "UTF-8"), TraceableMessage.class);
                 NewCCLinkEvent hipLinkEvent = mapper.convertValue(traceableMessage.getMessage(), NewCCLinkEvent.class);
-                logger.debug("Received message {}", traceableMessage);
+                logger.debug("Received Link Event message {}", hipLinkEvent);
                 subscriptionManager.notifySubscribers(hipLinkEvent);
             } catch (Exception e) {
                 logger.error(e.getMessage(), e);
