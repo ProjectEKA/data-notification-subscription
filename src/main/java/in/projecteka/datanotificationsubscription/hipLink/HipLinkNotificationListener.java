334bb3f352e4300bb4523679c89358629a904b2d
MD Dubey <mddubey409@gmail.com>
2021-03-01 13:18:02 +0530

    Dubey | 000 | Move to manual acknowledge

--- src/main/java/in/projecteka/datanotificationsubscription/hipLink/HipLinkNotificationListener.java
+++ src/main/java/in/projecteka/datanotificationsubscription/hipLink/HipLinkNotificationListener.java
@@ -5,12 +5,14 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import in.projecteka.consentmanager.common.TraceableMessage;
 import in.projecteka.datanotificationsubscription.HIUSubscriptionManager;
 import in.projecteka.datanotificationsubscription.ListenerProperties;
+import in.projecteka.datanotificationsubscription.common.ClientError;
 import in.projecteka.datanotificationsubscription.common.Serializer;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import reactor.core.publisher.Mono;
+import reactor.rabbitmq.AcknowledgableDelivery;
 import reactor.rabbitmq.Receiver;
 import reactor.util.retry.Retry;
 import reactor.util.retry.RetryBackoffSpec;
@@ -31,16 +33,17 @@ public class HipLinkNotificationListener {
 
     @PostConstruct
     public void subscribe() {
-        receiver.consumeAutoAck(HIP_LINK_QUEUE)
+        receiver.consumeManualAck(HIP_LINK_QUEUE)
                 .subscribe(delivery -> {
                             TraceableMessage traceableMessage = Serializer.to(delivery.getBody(), TraceableMessage.class);
                             Mono.just(traceableMessage)
                                     .map(this::extractLinkEvent)
                                     .doOnNext(linkEvent -> logger.info("Received link event for health-id-number {} from HIP {}", linkEvent.getHealthNumber(), linkEvent.getHipId()))
                                     .flatMap(newCCLinkEvent -> subscriptionManager.notifySubscribers(newCCLinkEvent).then())
+                                    .doOnSuccess(unused -> delivery.ack())
                                     .doOnError(throwable -> logger.error("Error while processing link event", throwable))
                                     .doFinally(signalType -> MDC.clear())
-                                    .retryWhen(retryConfig())
+                                    .retryWhen(retryConfig(delivery))
                                     .subscriberContext(ctx -> ctx.put(CORRELATION_ID, traceableMessage.getCorrelationId()))
                                     .subscribe();
                         }
@@ -59,15 +62,13 @@ public class HipLinkNotificationListener {
         return mapper.convertValue(traceableMessage.getMessage(), NewCCLinkEvent.class);
     }
 
-    private RetryBackoffSpec retryConfig() {
+    private RetryBackoffSpec retryConfig(AcknowledgableDelivery delivery) {
         return Retry
-                .fixedDelay(listenerProperties.getLinkEventMaximumRetries(), Duration.ofMillis(listenerProperties.getLinkEventRetryInterval()))
-                .doAfterRetry(retrySignal -> {
-                    logger.error("Failed retry number {}", retrySignal.totalRetries() + 1);
-                    if ((retrySignal.totalRetries() + 1) == listenerProperties.getLinkEventMaximumRetries()) {
-                        logger.info("Exhausted {} retries", listenerProperties.getLinkEventMaximumRetries());
-                        //TODO: Should it go to DLQ or Parking
-                    }
+                .fixedDelay(listenerProperties.getLinkEventMaximumRetries(), Duration.ofMillis(listenerProperties.getLinkEventMaximumRetries()))
+                .onRetryExhaustedThrow((retryBackoffSpec, retrySignal) -> {
+                    logger.info("Exhausted Retries");
+                    delivery.nack(false);
+                    return ClientError.unknownErrorOccurred();
                 });
     }
 }
